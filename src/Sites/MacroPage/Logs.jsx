7e3f2a728979eeca0140cc1e37408a84332c15e6
Ryuichi Nakamura <Ryuichi.rooochie@gmail.com>
2024-07-17 13:04:49 +1000

    Styling changes on the logs section of Macros

--- src/Sites/MacroPage/Logs.jsx
+++ src/Sites/MacroPage/Logs.jsx
@@ -2,25 +2,56 @@ import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
 import { rmLog } from "../../API/Slices/MacroLogSlice";
 
-const LogsPage = styled.div``;
+const LogsPage = styled.div`
+    margin-top: 25px;
+`;
+const Header = styled.div`
+    width: 100%;
+    padding-right: 80px;
+    display: inline-grid;
+    grid-template-columns: 40% 15% 15% 15% 15%;
+    grid-template-rows: 25px;
+    border-bottom: 1px solid black;
+`;
+
+const Headings = styled.h4`
+    margin: auto;
+    height: 100%;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+`;
+
+const LogsContainer = styled.div``;
 const LogsSpan = styled.span`
+    padding-right:80px;
     width: 100%;
     height: 40px;
-    display: flex;
+    display: grid;
     align-items: center;
-    padding-left: 15px;
+    grid-template-columns: 40% 15% 15% 15% 15% 80px;
     &:hover {
         background-color: lightgrey;
         border-radius: 5px;
     }
+    .numbers {
+        display: flex;
+        justify-content: center;
+    }
 `;
-const LogsResult = styled.p``;
+const LogsResult = styled.p`
+    margin-left: 15px;
+`;
+const Calories = styled.p``;
+const Proteins = styled.p``;
+const Fats = styled.p``;
+const Carbs = styled.p``;
+
 const ClearItem = styled.button`
     height: 35px;
     width: 50px;
-    margin: 0 15px 0 auto;
+    margin: auto 15px auto auto;
 `;
-
 export default function Logs() {
     const dispatch = useDispatch();
 
@@ -36,6 +67,10 @@ export default function Logs() {
         return (
             <LogsSpan key={index}>
                 <LogsResult >{logs.title}</LogsResult>
+                <Calories className="numbers">{logs.calories}</Calories>
+                <Proteins className="numbers">{logs.protein}</Proteins>
+                <Fats className="numbers">{logs.fats}</Fats>
+                <Carbs className="numbers">{logs.carbohydrates}</Carbs>
                 <ClearItem onClick={() => handleRmItem(logs)}>X</ClearItem>
             </LogsSpan>
         )
@@ -43,7 +78,16 @@ export default function Logs() {
 
     return(
         <LogsPage>
-            {logsList}
+            <Header>
+                <Headings>Name</Headings>
+                <Headings>Calories</Headings>
+                <Headings>Protein</Headings>
+                <Headings>Fats</Headings>
+                <Headings>Carbohydrates</Headings>
+            </Header>
+            <LogsContainer>
+                {logsList}
+            </LogsContainer>
         </LogsPage>
     )
 }
